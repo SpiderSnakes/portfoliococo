8e020521f6b1eed2c9fc99434c8534b1484f2b30
Cyprien RIVIERE--CACHAU <89699299+SpiderSnakes@users.noreply.github.com>
2025-11-19 15:04:54 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: 'standalone',
   trailingSlash: true,
   images: {
     unoptimized: true,
